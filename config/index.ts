4b5c2ac0ca531f9ce254ed8bfb4aa849f5373406
jememouse <jememouse@gmail.com>
2024-09-01 04:02:26 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -4,21 +4,6 @@ export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const IS_WORKFLOW = `${process.env.NEXT_PUBLIC_APP_TYPE_WORKFLOW}` === 'true'
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
-  description: 'App description',
-  copyright: '',
-  privacy_policy: '',
-  default_language: 'zh-Hans',
-}
-
-export const API_PREFIX = '/api'
-
-export const LOCALE_COOKIE_NAME = 'locale'
-
-export const DEFAULT_VALUE_MAX_LEN = 48
-
-
-export const APP_INFO: AppInfo = {
   title: 'Chat APP',
   description: '',
   copyright: '',
@@ -28,3 +13,10 @@ export const APP_INFO: AppInfo = {
 
 export const isShowPrompt = true
 export const promptTemplate = ''
+
+
+export const API_PREFIX = '/api'
+
+export const LOCALE_COOKIE_NAME = 'locale'
+
+export const DEFAULT_VALUE_MAX_LEN = 48
